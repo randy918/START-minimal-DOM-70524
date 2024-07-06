c034637272f4fda6f74bd405b67a22f4da6a394b
Randy Miller <randy918@gmail.com>
2024-07-06 12:00:10 -0500

    click in html sample 7-06-24-1200

--- js/index.js
+++ js/index.js
@@ -7,6 +7,20 @@
 
 //< ████████████████████████████████████  FUNCTIONS
 
+function newWindow()  {
+
+const url = 'https://dictionary.com/browse/shivoo'
+
+const newWindow = window.open(url, '','width = 500, height = 500');
+
+setTimeout(() =>  {
+
+newWindow.close();
+
+}, 5000);
+
+}
+
 
 //_$ ████████████████████████████████████  QUERY SELECTORS
 
