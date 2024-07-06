ef90f456069a56e9f613a0835c314fd84ea965d5
Randy Miller <randy918@gmail.com>
2024-07-05 23:58:57 -0500

    7-05-24-2358

--- js/index.js
+++ js/index.js
@@ -1,24 +1,12 @@
 //?  ████████████████████████████████████  DATA
 
-const endpoint = 'https://gist.githubusercontent.com/ Miserlou/c5cd8364bf9b2420bb29/ raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json'
-
-const cities = [];
 
 //_ ████████████████████████████████████  PROMISES
 
-fetch(endpoint) 
-.then(blob => blob.json())
-.then(data => cities.push(...data))
 
 
-//_ ████████████████████████████████████  FUNCTIONS
+//< ████████████████████████████████████  FUNCTIONS
 
-function findMatches(wordToMatch, cities) {
-
-  const regex = new RegExp(wordToMatch, 'gi');
-  return placeText.city.match(regex) || placeText.state.match(regex)
-
-});
 
 //_$ ████████████████████████████████████  QUERY SELECTORS
 
