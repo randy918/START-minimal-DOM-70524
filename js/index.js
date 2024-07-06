c8be32b2f6ae0dfa78cbabf1fe0bae451d996bdd
Randy Miller <randy918@gmail.com>
2024-07-06 17:07:37 -0500

    selectors ad element getting done right! 7-06-24-1707

--- js/index.js
+++ js/index.js
@@ -7,15 +7,56 @@
 
 //< ████████████████████████████████████  FUNCTIONS
 
- 7-06-24-1200 
+//< ████████████████████████████████████  SECTION 1
+
+let header = document.getElementById('header');
+header.style.backgroundColor = 'yellow';
+
+//< ████████████████████████████████████  SECTION 2
+
+let classElements = document.getElementsByClassName('header');
+for (let i = 0; i < classElements.length; i++) {
+
+    document.getElementsByClassName('header')[i].style.backgroundColor = 'yellow';
+
+}
+
+//< ████████████████████████████████████  SECTION 3
+
+let tagNames = document.getElementsByTagName('p');
+for (let i = 0; i < tagNames.length; i++) {
+
+    document.getElementsByTagName('p')[i].style.backgroundColor = 'yellow';
+}
+
+
+//< ████████████████████████████████████  SECTION 4
+
+let singleQuery = document.querySelector('#query');
+
+document.querySelector('#query').style.backgroundColor = 'yellow';
+
+
+
+//< ████████████████████████████████████  SECTION 5
+
+let allQuery = document.querySelectorAll('.query-all');
+
+
+for(let i = 0; i < allQuery.length; i++){
+    
+    document.querySelectorAll('.query-all')[i].style.backgroundColor = 'yellow';
+
+
+}
 
 //_$ ████████████████████████████████████  QUERY SELECTORS
 
-//^ ████████████████████████████████████  EVENT LISTENERS
+//^  ████████████████████████████████████  EVENT LISTENERS
 
 //>  ████████████████████████████████████  REAL PROGRAM
 
-//! ████████████████████████████████████  FILTER array example [filter results]
+//!  ████████████████████████████████████  FILTER array example [filter results]
 
 
 
